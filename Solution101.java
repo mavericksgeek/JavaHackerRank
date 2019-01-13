b19b5320fc3e94dabccabdfe1c5c660e1e91e761
mavericksgeek <sarsekar@islander.tamucc.edu>
2019-01-13 16:46:06 -0600

    Create Solution101.java

---
+++ Solution101.java
@@ -0,0 +1,18 @@
+/* Since the first few are simple problems, I will not describe the file content
+Problem 101
+You must print two lines of output:
+
+Print Hello, World. on the first line.
+Print Hello, Java. on the second line.
+*/
+
+public class Solution101 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+/*
